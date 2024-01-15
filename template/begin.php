dfe6bcc1feb267f478b860f73c30f77eed361e11
arxofficial2411 <arxofficial241105@gmail.com>
2024-01-15 23:15:22 +0700

    Mengubah metadata

--- template/begin.php
+++ template/begin.php
@@ -1,3 +1,16 @@
-<?php
-	
-?>
+<!doctype html>
+<html lang="id">
+<head>
+  <meta charset="utf-8">
+  <meta content="initial-scale=1.0, width=device-width" name="viewport">
+  <meta content="ie=edge" http-equiv="X-UA-Compatible">
+  <meta content="" name="application-name">
+  <meta content="" name="author">
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+  <link href="" rel="apple-touch-icon">
+  <link href="" rel="icon">
+  <title>Judul</title>
+  <!-- Framework Modules -->
+</head>
+<body>
